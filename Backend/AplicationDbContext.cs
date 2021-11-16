0e8936b8e0cdb6081a3e47b87f9c42d803a54d05
RenzoElias <renzoeliasdelacalle@gmail.com>
2021-11-15 19:40:33 -0500

    init app

---
+++ Backend/AplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class AplicationDbContext: DbContext
+    {
+        public DbSet<Comentario> Comentario { get; set; }
+        // SQL Server - appsettings // "DefaultConnection": "Server=DESKTOP-FU3S2O4;Database=IngresoGastoDB;Trusted_Connection=True;MultipleActiveResultSets=true"
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
---
+++ Backend/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Backend", Version = "v1" });
+            });
+            //Cadena de conexion
+            string mySqlConnection = Configuration.GetConnectionString("DefaultConnection");
+            //Usar MySQL
+            services.AddDbContext<AplicationDbContext>(options =>
+                options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+            //Luego PM> Add-Migration v1.0.0
+            //Para que solo genere la carpeta Migration y haga la creacion de la tabla, segun el Model
+            //Luego PM> Update-Database
+            //Para que haga la migracion y cree la BD en MySQL
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Backend v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
